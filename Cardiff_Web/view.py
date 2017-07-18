a6266cff01b076be295cf907c97558e176d3216b
Tony Su <peitaosu@163.com>
2017-07-18 22:14:40 +0800

    add method response_console_output to redirect console output to HttpResponse

--- Cardiff_Web/view.py
+++ Cardiff_Web/view.py
@@ -11,8 +11,16 @@ cardiff = Cardiff()
 cardiff_settings_path = os.path.join(cardiff_path, "settings.json")
 cardiff.load_settings(cardiff_settings_path)
 
+
+def response_console_output(func):
+    def new_func(*args, **kwargs):
+        console_output = StringIO.StringIO()
+        sys.stdout = console_output
+        func(*args, **kwargs)
+        sys.stdout = sys.__stdout__
+        return HttpResponse(['<BR>' if char == '\n' else char for char in console_output.getvalue()])
+    return new_func
+
+@response_console_output
 def info(request):
-    origin_stdout = sys.stdout
-    sys.stdout = s = StringIO.StringIO()
     cardiff.exec_cmd(["info"])
-    return HttpResponse(s.getvalue())
